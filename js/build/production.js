a8f1efdaa008f464a05796e80f1bdf9d6d267a71
quattromani <max@maxquattromani.com>
2014-11-27 14:39:02 -0700

    added some more lame excuses

--- js/build/production.js
+++ js/build/production.js
@@ -17,6 +17,16 @@ var mediumBreakPoint = 768;
     excuses[2] = "I'll be giving my landscaper a tour of the museum";
     excuses[3] = "I have to pickup a football from my mechanic (long story!)";
     excuses[4] = "I need to work on my personal novella based on the life and times of my gerbil, Mr. Pickles III";
+    excuses[5] = "my fish is sick.";
+    excuses[6] = "I can't find my shoes.";
+    excuses[7] = "YOLO!";
+    excuses[8] = "I'm renting my Llama out for a birthday party.";
+    excuses[9] = "I accidentally got on a plane.";
+    excuses[10] = "I’m stuck in the blood pressure machine at the grocery store.";
+    excuses[11] = "A tree fell on my car.";
+    excuses[12] = "YOLO!";
+    excuses[13] = "I'm renting my Llama out for a birthday party.";
+    excuses[14] = "My cat seems lonely.";
 
     var quote = excuses[Math.floor(Math.random() * excuses.length)];
 
